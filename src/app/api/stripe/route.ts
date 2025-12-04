455705b7f4ed48a0d932f347d1da7c8ff1ffdbf6
Michael Nam <michael@Michaels-MacBook-Pro-2.local>
2025-12-05 01:28:57 +0900

    fix stripe api version

--- src/app/api/stripe/route.ts
+++ src/app/api/stripe/route.ts
@@ -4,7 +4,7 @@ import { cookies } from 'next/headers'
 import { NextRequest, NextResponse } from 'next/server'
 import Stripe from 'stripe'
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, { apiVersion: '2024-06-20' })
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, { apiVersion: '2025-11-17.clover' })
 
 export async function POST(request: NextRequest) {
   const { price_id } = await request.json()
